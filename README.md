bcf1512dd5005eaa77d904fbc641a80fe6dc5025
Cray077 <bernando2345@gmail.com>
2022-02-07 18:42:37 +0700

    Added Search Function

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Library-Management
-App for managing books
+App for managing books built with python ,tkinter, and sqlite3
+
+This app it's a project for my school, they gave me a task to make
+an application to manage books on the school library, and since i'm new
+at programming i made this project a practice for my programming skills.
--- main_gui.py
+++ main_gui.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from datetime import datetime
+from datetime import date
 import sqlite3
 
 # SQL Variables
@@ -40,12 +40,6 @@ def delete_book(book):
         cur.execute("DELETE FROM books WHERE title= :title AND id_num= :id_num",
                 {"title": book[0], "id_num": book[4]})
 
-def search_database(key, value):
-    pass
-
-def sort_database():
-    pass
-
 # Create Table in SQL
 try:
     create_table()
@@ -53,16 +47,12 @@ except sqlite3.OperationalError:
     print("Okay")
 
 
-####################################################################################################
-#SEPARATOR                                                                                         
-####################################################################################################
+######################################### SEPARATOR ###########################################################
 # Main GUI
 root = Tk()
-
 root.title("Library Book's Management")
 
 # Frame
-
 entry = LabelFrame(root, padx=10, pady=10)
 treeview = LabelFrame(root)
 
@@ -73,7 +63,7 @@ tree = ttk.Treeview(treeview)
 tree["columns"] = ("Title", "Author", "Year", "Isbn", "ID", "Time", "Description")
 
 tree.column("#0", width=35, stretch=False, anchor=W)
-tree.column("Title", width=120, stretch=False, anchor=W)
+tree.column("Title", width=90, stretch=False, anchor=W)
 tree.column("Author", width=20, stretch=False, anchor=W)
 tree.column("Year", width=50, stretch=False, anchor=W)
 tree.column("Isbn", width=70, stretch=False, anchor=W)
@@ -91,7 +81,7 @@ tree.heading("ID", text = "ID")
 tree.heading("Time", text = "Time", anchor=W)
 tree.heading("Description",text = "Description", anchor=W)
 
-# Scrollbar
+######################## Scrollbar ###############################
 # Scrollbar Vertical
 scrollbar = ttk.Scrollbar(treeview, orient="vertical", command=tree.yview)
 tree.configure(yscroll=scrollbar.set)
@@ -101,11 +91,17 @@ scrollbar_hor = ttk.Scrollbar(treeview, orient="horizontal", command=tree.xview)
 tree.configure(xscroll=scrollbar_hor.set)
 
 # Insert Data to Treeview
-database = read_database()
-num = 1
-for x in database:
-    tree.insert(parent= "", index= "end", iid= num, text= num,values = (x[0],x[1],x[2],x[3],x[4],x[5],x[6]))
-    num += 1
+def insert_data():
+    database = read_database()
+    num = 1
+    for x in database:
+        tree.insert(parent= "", index= "end", iid= num, text= num,values = (x[0],x[1],x[2],x[3],x[4],x[5],x[6]))
+        num += 1
+insert_data()
+
+def remove_data():
+    for x in tree.get_children():
+        tree.delete(x)
 
 # Entry
 title = Entry(entry, width=30)
@@ -114,6 +110,8 @@ year = Entry(entry, width=30)
 isbn = Entry(entry, width=30)
 id_num = Entry(entry, width=30)
 descript = Entry(entry, width=30)
+
+search_bar = Entry(treeview, width= 50)
 
 #Label
 
@@ -126,19 +124,28 @@ desc_lb = Label(entry, text="Description", anchor="w")
 
 
 # Functions
+def clear_entry():
+    title.delete(0, END)
+    author.delete(0, END)
+    year.delete(0,END)
+    isbn.delete(0,END)
+    id_num.delete(0, END)
+    descript.delete(0, END)
+
 def save():
     data1 = title.get()
     data2 = author.get()
     data3 = year.get()
     data4 = isbn.get()
     data5 = id_num.get()
-    data6 = datetime.today()
+    data6 = date.today()
     data7 = descript.get()
 
     data = (data1,data2,data3,data4,data5, data6, data7)
     add_book(data)
     database = read_database()
     num = (len(database))
+    clear_entry()
 
     tree.insert(parent= "", index= "end", iid= num, text= num,values = (data1, data2, data3, data4, data5, data6, data7))
 
@@ -161,7 +168,6 @@ def edit():
     isbn.insert(0, data[3])
     id_num.insert(0, data[4])
     descript.insert(0, data[6])
-    btn_save = Button(entry, text="Update", command = update, width=10)
 
 def update():
     select = tree.focus()
@@ -172,7 +178,7 @@ def update():
     year.get(),
     isbn.get(),
     id_num.get(),
-    datetime.today(),
+    date.today(),
     descript.get()
     ))
     with conn:
@@ -183,17 +189,12 @@ def update():
                     "year": year.get(),
                     "isbn": isbn.get(),
                     "id_num": id_num.get(),
-                    "datetime": datetime.today(),
+                    "datetime": date.today(),
                     "descript": descript.get()
                     })
 
-    title.delete(0, END)
-    author.delete(0, END)
-    year.delete(0,END)
-    isbn.delete(0,END)
-    id_num.delete(0, END)
-    descript.delete(0, END)
 
+    clear_entry()
 
 
 def delete():
@@ -203,6 +204,22 @@ def delete():
         delete_book(book)
         tree.delete(x)
 
+def search():
+    key = "author"
+    value = search_bar.get()
+    data = read_database()
+    ls = ("title", "author", "year", "isbn", "id_num", "datetime", "descript")
+    found = []
+    for x in data:
+        if value.lower() in x[ls.index(key)].lower():
+            found.append(x)
+    remove_data()
+    num = 1
+    for x in found:
+        tree.insert(parent= "", index= "end", iid= num, text= num,values = (x[0],x[1],x[2],x[3],x[4],x[5],x[6]))
+        num += 1
+
+
 
 # Buttons
 btn_save = Button(entry, text="Save", command = save, width=10)
@@ -210,15 +227,19 @@ btn_read = Button(entry, text="Read", command = read, width=10)
 btn_edit = Button(entry, text="Edit", command = edit, width=10)
 btn_delete = Button(entry, text="Delete", command = delete, width=10)
 btn_update = Button(entry, text="Update", command = update, width=10)
+btn_search = Button(treeview, text="Search", command=search, width=10)
+
 
 # Grid
 entry.grid(row=0, column=1)
 treeview.grid(row=0, column=0)
 
-tree.grid(row=0, column= 0)
+btn_search.grid(row=0, column=1)
+search_bar.grid(row=0, column=0)
+tree.grid(row=1, column= 0, columnspan=2)
 
-scrollbar.grid(row=0, column=1, sticky="ns")
-scrollbar_hor.grid(row=1, column=0, sticky="ew")
+scrollbar.grid(row=1, column=2, sticky="ns")
+scrollbar_hor.grid(row=2, column=0, sticky="ew", columnspan=2)
 
 title.grid(row = 1, column = 0, columnspan = 3)
 author.grid(row = 3, column = 0, columnspan = 3)
@@ -239,6 +260,7 @@ year_lb.grid(row = 4, column = 0)
 isbn_lb.grid(row = 6, column = 0) 
 id_lb.grid(row = 8, column = 0) 
 desc_lb.grid(row = 10, column = 0)  
+
 
 
 root.mainloop()
